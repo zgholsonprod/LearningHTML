2e2ba5bcab5d599abc9c8facec46094a9bd8d513
zgholsonprod <53591121+zgholsonprod@users.noreply.github.com>
2020-09-04 14:33:32 -0500

    Ages and Stalker Exercise
    Ages and Stalker Exercise

---
+++ LearningCSS/Javascript/StalkerExercise/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old.");
+
+var ageInDays = age * 365.25;
+console.log("You are " + ageInDays + " days old.");
